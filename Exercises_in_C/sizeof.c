538079800e4ac9d1080df9d298fd916a7d65c1fe
Mark Kibara <markkibara@Marks-MacBook-Air.local>
2022-10-22 16:55:05 +0300

    commit

--- Exercises_in_C/sizeof.c
+++ Exercises_in_C/sizeof.c
@@ -3,6 +3,7 @@
 int main(void){
 	printf("Char size: %lu bytes\n", sizeof(char));
 	printf("int size: %lu bytes\n", sizeof(int));
+	printf("unsigned int size: %lu bytes\n", sizeof(unsigned int));
 	printf("short size: %lu bytes\n", sizeof(short));
 	printf("long size: %lu bytes\n", sizeof(long));
 	printf("float size: %lu bytes\n", sizeof(float));
